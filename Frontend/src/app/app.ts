bfcdf137dc34ae8b9493de98498e30d6694d0e3b
Joshua Miller <84877484+joshakaw@users.noreply.github.com>
2026-04-20 15:54:18 -0400

    Project set up - Install Angular Material - Migrate from css to scss

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar'
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.html',
-    styleUrl: './app.css',
-    imports: [RouterOutlet]
+    styleUrl: './app.scss',
+    imports: [RouterOutlet, MatToolbar]
 })
 export class App {
   protected readonly title = signal('Frontend');
--- Frontend/src/dashboard-component/dashboard-component.ts
+++ Frontend/src/dashboard-component/dashboard-component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCard, MatCardTitle, MatCardContent } from "@angular/material/card";
 
 @Component({
   selector: 'app-dashboard-component',
-  imports: [],
+  imports: [MatGridListModule, MatCard, MatCardTitle, MatCardContent],
   templateUrl: './dashboard-component.html',
   styleUrl: './dashboard-component.css',
 })
 export class DashboardComponent {
+  public inpatients : Array<{name: string, status: string}>
 
+  constructor(){
+    this.inpatients = [
+      {name: "Adam", status: "Urgent"}, {name: "Bill", status: "Non-Urgent"}
+    ]
+  }
 }
